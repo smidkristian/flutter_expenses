27af00ab928955d9c92dd13a2c234a203e46b5ec
smidkristian <smid.kristian@gmail.com>
2021-04-02 20:28:52 +0200

    initial

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Expenses Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Expenses'),
+        ),
+        body: Center(
+          child: Text('keep track of your expenses'),
+        ));
+  }
+}
